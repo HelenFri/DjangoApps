607581f0f4b22d95c0ff0803e5e8e6f63e406c8a
Елена <125108381+HelenFri@users.noreply.github.com>
2023-10-03 17:17:47 +0700

    Registration page was added.

--- mynotes/mainapp/views.py
+++ mynotes/mainapp/views.py
@@ -4,3 +4,17 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'mainapp/home.html', context={'page': home})
 
+
+def reg(request):
+    if request.method == 'GET':
+        return render(request, 'mainapp/reg.html')
+
+    data = request.POST
+    username = data.get('username')
+    first_name = data.get('first_name')
+    last_name = data.get('last_name')
+    email = data.get('email')
+    pas1, pas2 = data.get('password1'), data.get('password2')
+
+    return render(request, 'mainapp/reg.html', context={'page': reg})
+
