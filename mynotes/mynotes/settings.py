a5d0e4acaa1988a91f150d683f07d6e1a130cf72
Елена <125108381+HelenFri@users.noreply.github.com>
2023-10-04 16:52:31 +0700

    db.sqlite3 was deleted. Static folders was added in settings.

--- mynotes/mynotes/settings.py
+++ mynotes/mynotes/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 from django.contrib.messages import constants as messages
@@ -132,6 +132,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIR = []
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
